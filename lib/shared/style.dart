51be27212cc45ab5546477ebb7624df3e91bd76a
mudapa <mudanpa@gmail.com>
2024-07-08 21:22:37 +0700

    add google_fonts & style

---
+++ lib/shared/style.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor = const Color(0xffFFFFFF);
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff030711);
+Color greyColor = const Color(0xffA4A8AE);
+Color placeholderColor = const Color(0xffA4A8B5);
+Color primaryColor = const Color(0xff0072FF);
+
+TextStyle title = GoogleFonts.mulish(
+  fontSize: 20,
+);
+
+TextStyle subtitle = GoogleFonts.mulish(
+  fontSize: 18,
+);
+
+TextStyle body = GoogleFonts.mulish(
+  fontSize: 14,
+);
