ba80b08a45e7ffdafcb86ea51eaf17c9d55bfa4a
ginginginanjar063@gmail.com <ginginginanjar063@gmail.com>
2024-07-13 00:36:28 +0700

    Progres2

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:mini_project_team_7/bloc/product_bloc/product_bloc.dart';
 import 'package:mini_project_team_7/model/product_model.dart';
+import 'package:mini_project_team_7/shared/style.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -9,100 +11,143 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     context.read<ProductBloc>().add(LoadProductEvent());
-    return Scaffold(
-      appBar: AppBar(title: const Text('Product')),
-      body: BlocBuilder<ProductBloc, ProductState>(
-        builder: (context, state) {
-          if (state is ProductLoadingState) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is ProductLoadedState) {
-            var data = state.products;
-            return Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 10.0,
-                  ),
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    var product = data[index] as ProductModel;
-                    return Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.0),
-                        border: Border.all(
-                          color: Colors.blue,
-                          width: 3.0,
+    return DefaultTabController(
+      length: 5,
+      initialIndex: 0,
+      child: Scaffold(
+        backgroundColor: whiteColor,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'STORE',
+            style: title.copyWith(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: whiteColor,
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+          ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(50.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  TabItem('Fashion'),
+                  TabItem('Shoes'),
+                  TabItem('Food'),
+                  TabItem('Electronic'),
+                  TabItem('Sport'),
+                ],
+              ),
+            ),
+          ),
+        ),
+        drawer: Drawer(),
+        body: BlocBuilder<ProductBloc, ProductState>(
+          builder: (context, state) {
+            if (state is ProductLoadingState) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is ProductLoadedState) {
+              var data = state.products;
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: MasonryGridView.builder(
+                    gridDelegate:
+                        const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      var product = data[index] as ProductModel;
+                      return Container(
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                            color: primaryColor.withOpacity(0.2),
+                            width: 5.0,
+                          ),
                         ),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Image.network(
-                            product.image,
-                            height: 150,
-                            fit: BoxFit.cover,
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            product.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Image.network(
+                              product.image,
+                              height: 150,
+                              fit: BoxFit.fitWidth,
                             ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            product.description,
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey[600],
+                            SizedBox(height: 10),
+                            Text(
+                              product.title,
+                              style: title,
+                              textAlign: TextAlign.start,
                             ),
-                            textAlign: TextAlign.center,
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                product.price.toString(),
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 6, vertical: 2),
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Text(
-                                  product.rating.toString(),
+                            SizedBox(height: 10),
+                            Text(
+                              product.description,
+                              style: subtitle.copyWith(
+                                  fontSize: 14, color: Colors.grey[600]),
+                              textAlign: TextAlign.start,
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Text(
+                                  '\$${product.price}',
                                   style: TextStyle(
-                                    color: Colors.white,
+                                    fontSize: 18,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-            );
-          } else if (state is ProductErrorState) {
-            return const Center(child: Text('Failed to fetch cart'));
-          } else {
-            return const SizedBox();
-          }
-        },
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.star, color: Colors.amber),
+                                    SizedBox(width: 2),
+                                    Text(
+                                      product.rating.rate.toString(),
+                                      style: TextStyle(
+                                        color: blackColor,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+              );
+            } else if (state is ProductErrorState) {
+              return const Center(child: Text('Failed to fetch cart'));
+            } else {
+              return const SizedBox();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Container TabItem(String nameTab) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 6.0),
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      child: Tab(
+        child: Text(
+          nameTab,
+          style: title.copyWith(color: whiteColor),
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
